b8b1b4089e58869851b5c7bf66d02254fc7a0a84
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-11-24 21:05:08 +0100

    first commit

---
+++ static/js/home.js
@@ -0,0 +1,22 @@
+// Challenge 1: age in days.
+
+function ageInDays(){
+    let age = prompt('what year were you born?');
+    let days = (2019 - age) * 365;
+    let h1 = document.createElement('h1');
+    let textArea = document.createTextNode('You are '+days+' days old');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textArea);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
+
+function generateCat(){
+let image = document.createElement('img');
+let div = document.getElementById('flex-cat-gen');
+image.src ='https://cataas.com//cat/gif';
+div.appendChild(image);
+}
